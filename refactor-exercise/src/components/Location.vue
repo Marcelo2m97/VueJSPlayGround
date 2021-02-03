418f75d01f421ee6a054f2a962f084bafe0f48ff
Marcelo2m97 <marcelo2m1097@gmail.com>
2021-02-03 14:53:18 -0600

    Add refactored code to use Vue CLI

---
+++ refactor-exercise/src/components/Location.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="place">
+    <img :src="destiny.img" width="235" height="300" />
+    <h2>{{ destiny.name }}</h2>
+    <p>{{ destiny.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["destiny"],
+  template: "#destinationcomponent"
+};
+</script>
+
+<style lang="scss" scoped>
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 330px;
+  height: 450px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+  h2 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    margin: 10px;
+    align-self: center;
+  }
+}
+</style>
